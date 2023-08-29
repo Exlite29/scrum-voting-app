888ac1467173902cff3b4b0d743105737d5cd02e
Exlite29 <arielagdon@yahoo.com>
2023-08-29 12:09:45 +0800

    second update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,11 @@
-import logo from "../logo.svg";
 import "./App.css";
+import Heading from "../components/Heading";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Arie Agdon
-        </a>
-      </header>
-    </div>
+    <main className="max-w-7xl mx-auto my-5 px-4 sm:px-6 lg:px-8">
+      <Heading />
+    </main>
   );
 }
 
